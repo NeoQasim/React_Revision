f4140165fdf1cf17cd7d28e21c620c317c0eacca
neoqasim <neoqasim@gmail.com>
2023-09-19 23:52:07 +0500

    tried more

--- src/Probgchanger.jsx
+++ src/Probgchanger.jsx
@@ -19,7 +19,7 @@ const Probgchanger = () => {
             <div className="w-full h-screen duration-1000" style={{ backgroundColor: color }}></div>
             <div className="fixed gap-8 bg-white w-2/4 py-2 rounded-3xl  mx-auto inset-x-0 flex flex-wrap bottom-8 justify-center">
                 {colors.map((coloroptions) => (
-                    <button
+                    <button 
                         key={coloroptions.color}
                         onClick={() => handlecolor(coloroptions.color)} className={`btn bg-${coloroptions.color}-600  bg-bl text-white px-4 py-0.5 rounded-3xl`}
                     >
